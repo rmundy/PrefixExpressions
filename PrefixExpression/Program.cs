4acc21559393ce8c560a057ada78609cfa2f6248
Bobby Mundy <robert.l.mundy@gmail.com>
2016-01-23 14:03:41 -0500

    StyleCop Fixes

--- PrefixExpression/Program.cs
+++ PrefixExpression/Program.cs
@@ -1,15 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PrefixExpression
+﻿namespace PrefixExpression
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public sealed class Program
     {
         public static void Main(string[] args)
         {
+            using (var reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    String line = reader.ReadLine();
+                    if (null == line)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
