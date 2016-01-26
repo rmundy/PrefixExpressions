d25ad1b6c01d2efdc113fa0a930e736ad18d4606
Bobby Mundy <robert.l.mundy@gmail.com>
2016-01-25 21:13:39 -0500

    Solved

--- PrefixExpression/Program.cs
+++ PrefixExpression/Program.cs
@@ -1,53 +1,54 @@
-﻿namespace PrefixExpression
+﻿using System.Globalization;
+using System.Linq.Expressions;
+
+namespace PrefixExpression
 {
     using System;
     using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public sealed class Program
     {
-        public static void Main(string[] args)
+        public static void Main(String[] args)
         {
             using (var reader = File.OpenText(args[0]))
             {
                 while (!reader.EndOfStream)
                 {
-                    String line = reader.ReadLine();
+                    var line = reader.ReadLine();
                     if (null == line)
                     {
-                        continue;
                     }
                     else
                     {
-                        var split = line.Split(' ');
+                        var split = line.Split(new[]{' '}, StringSplitOptions.RemoveEmptyEntries);
                         var stack = new Stack<String>();
-                        var operators = new List<String>{"+", "*", "/"};
-                        for (int i = split.Length-1; i >= 0; i--)
+                        var operators = new List<String>{"+", "*", "/", "-"};
+                        for (var i = split.Length-1; i >= 0; i--)
                         {
                             if(operators.Contains(split[i]))
                             {
-                                if(stack.Count() >= 2)
+                                var num1 = Convert.ToDouble(stack.Pop());
+                                var num2 = Convert.ToDouble(stack.Pop());
+                                if (split[i].Equals("*"))
                                 {
-                                    var num1 = Convert.ToInt32(stack.Pop());
-                                    var num2 = Convert.ToInt32(stack.Pop());
-                                    if (split[i].Equals("*"))
-                                    {
-                                        var result = num1 * num2;
-                                        stack.Push(result.ToString());
-                                    }
-                                    else if (split[i].Equals("/"))
-                                    {
-                                        var result = num1 / num2;
-                                        stack.Push(result.ToString());
-                                    }
-                                    else if (split[i].Equals("+"))
-                                    {
-                                        var result = num1 + num2;
-                                        stack.Push(result.ToString());
-                                    }
+                                    var result = num1 * num2;
+                                    stack.Push(result.ToString(CultureInfo.InvariantCulture));
+                                }
+                                else if (split[i].Equals("/"))
+                                {
+                                    var result = num1 / num2;
+                                    stack.Push(result.ToString(CultureInfo.InvariantCulture));
+                                }
+                                else if (split[i].Equals("+"))
+                                {
+                                    var result = num1 + num2;
+                                    stack.Push(result.ToString(CultureInfo.InvariantCulture));
+                                }
+                                else if (split[i].Equals("-"))
+                                {
+                                    var result = num1 - num2;
+                                    stack.Push(result.ToString(CultureInfo.InvariantCulture));
                                 }
                             }
                             else
@@ -57,7 +58,7 @@
                             }
                         }
 
-                        Console.WriteLine(stack.Pop());
+                        Console.WriteLine("{0}",stack.Pop());
                     }
                 }
             }
